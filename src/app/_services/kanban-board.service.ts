94ba01c90ad5984794c296e26bfd7a7002b3a36d
ThéoEssoh <essohjean88@gmail.com>
2021-04-30 01:09:46 +0200

    add function list of caards

--- src/app/_services/kanban-board.service.ts
+++ src/app/_services/kanban-board.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs';
-import {BoardInfo, Kanban} from '../_models/kanban-model';
+import {BoardInfo, Card, Kanban} from '../_models/kanban-model';
+import {environment} from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -13,11 +14,20 @@ export class KanbanBoardService {
   rootUrl = '/kanban';
 
   getBoards(): Observable<Kanban[]> {
-    console.log(`The content of route: ${this.rootUrl}`);
-    return this.httpClient.get<Kanban[]>(this.rootUrl + '/boards');
+    /*console.log(`The content of route: ${this.rootUrl}`);*/
+    return this.httpClient.get<Kanban[]>( this.rootUrl + '/boards');
   }
 
   saveBoard(table: BoardInfo) {
-    return this.httpClient.post(this.rootUrl + '/board/add', table);
+    return this.httpClient.post( this.rootUrl + '/board/add', table);
+  }
+
+  getCards(): Observable<Card[]> {
+    /*console.log(`The content of route2: ${this.rootUrl}`);*/
+    return this.httpClient.get<Card[]>(this.rootUrl + '/cards');
+  }
+
+  saveCard(value: Card) {
+    return this.httpClient.post( this.rootUrl + '/board/add', value);
   }
 }
